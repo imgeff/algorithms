14bdcf2ea181ed592d2dc5a081e2aeb77fc93d76
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-30 16:58:43 -0300

    :recycle: refactor: Refatora função is_palindrome_recursive
    com o propósito de diminuir complexidade

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,14 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    try:
-        if high_index == 0:
+    if high_index >= 0:
+        if high_index <= low_index:
             return True
         if word[low_index] == word[high_index]:
             return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-        return False
-    except IndexError:
-        return False
-
-
-# if __name__ == "__main__":
-#     word = ""
-#     print(is_palindrome_recursive(word, 0, len(word) - 1))
+    return False
