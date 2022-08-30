0763cfefaa993e037aee0450f49e6ff2b0e82032
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-30 19:54:04 -0300

    :sparkles: feat: Implementa função is_palindrome_interative
    responsável por verificar se uma palavra é um palíndromo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    index_end = len(word)
+    if index_end > 0:
+        for letter in word:
+            index_end -= 1
+            if letter != word[index_end]:
+                return False
+        return True
+    return False
