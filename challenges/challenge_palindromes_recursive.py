2892871ddae4c39af10a3fc2d57be2c00f3d63c6
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-30 16:16:25 -0300

    :sparkles: feat: Implementa função is_palindrome_recursive
    responsável por verificar se uma palavra é um palíndromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if high_index < 0:
+        return False
+    if len(word) % 2 != 0:
+        if high_index == low_index:
+            return True
+        if word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        if high_index - low_index == -1:
+            return True
+        if word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
+
+
+# if __name__ == "__main__":
+#     word = ""
+#     print(is_palindrome_recursive(word, 0, len(word) - 1))
