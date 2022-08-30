1b8d94f48f43d615911b76c3b21487393fd203ec
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-30 18:06:01 -0300

    :sparkles: feat: Implementa função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+def is_anagram(first_string: str, second_string):
+    if len(first_string) == len(second_string):
+        first_string, second_string = (
+            first_string.lower(),
+            second_string.lower()
+        )
+        for letter in first_string:
+            second_string = second_string.replace(letter, "", 1)
+        if len(second_string) == 0:
+            return True
+    return False
+
+
+# if __name__ == "__main__":
+#     print(is_anagram("pedra", "perda"))
